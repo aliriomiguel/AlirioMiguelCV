6e2d40433a50963bf293e5c93450509eee48abe4
aliriomiguel <aliriomiguel@aranguren.us>
2023-04-01 20:57:05 +0200

    added components, fixed css issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ResumeComponent } from './resume/resume.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResumeComponent,
+    PortfolioComponent,
+    BlogComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
